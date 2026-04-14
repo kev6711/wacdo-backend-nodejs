81933700bb4dc02498c57304dbf478457d10d7cd
kev6711 <kevin.heckenmeyer@gmail.com>
2026-04-14 10:47:50 +0200

    Mise en place architecture MVC

--- app.js
+++ app.js
@@ -11,37 +11,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/wacdo/orders", (req, res) => {
-    res.status(200).json([
-        {
-            id: 1,
-            status: "AWAITING_PREPARATION",
-            user_id: 5,
-            date: "2026-04-08T16:00:00Z",
-            total_amount: 25,
-            order_lines: [
-                {
-                    id: 1,
-                    item_type: "PRODUCT",
-                    product_id: 1,
-                    menu_id: null,
-                    quantity: 1,
-                    unit_price: 10,
-                    total_price: 10,
-                },
-                {
-                    id: 2,
-                    item_type: "MENU",
-                    product_id: null,
-                    menu_id: 2,
-                    quantity: 1,
-                    unit_price: 15,
-                    total_price: 15,
-                },
-            ],
-        },
-    ]);
-});
+app.use("/wacdo/users", require("./routes/users.routes"));
 
 const startServer = async () => {
     try {
---
+++ routes/users.routes.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const { getUsers } = require("../controllers/users.controller");
+const router = express.Router();
+
+router.get("/", getUsers);
+
+module.exports = router;
